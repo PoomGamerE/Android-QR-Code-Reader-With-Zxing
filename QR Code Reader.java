0d11c4c1a75caa808e5a191fd372d5fed35c28bf
Poom Gamer Ember <poomgamere@lightkrub.rf.gd>
2020-05-08 00:46:07 +0700

    Update QR Code Reader.java

--- QR Code Reader.java
+++ QR Code Reader.java
@@ -1,1 +1,14 @@
-
+public static String ScanQR(Bitmap bitmap){
+        String Data = null;
+        int[] intArray = new int[bMap.getWidth() * bMap.getHeight()];
+        bMap.getPixels(intArray, 0, bMap.getWidth(), 0, 0, bMap.getWidth(), bMap.getHeight());
+        LuminanceSource source = new RGBLuminanceSource(bMap.getWidth(), bMap.getHeight(), intArray);
+        BinaryBitmap BB1 = new BinaryBitmap(new HybridBinarizer(source));
+        com.google.zxing.Result QRCode = null;
+        try {
+            QRCode = new QRCodeReader().decode(BB1);
+        } catch (Exception ignored) {
+        }
+        Data = QRCode.getText();
+        return Data;
+    }
